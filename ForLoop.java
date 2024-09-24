7c48d9d5d8142d73ca422cc99821bf4eb7116c85
Ahamed Basith <ahamedb254@gmail.com>
2024-09-24 10:44:40 +0530

    Add files via upload

---
+++ ForLoop.java
@@ -0,0 +1,27 @@
+package program;
+
+public class ForLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+//		(&&) (1.5) (1-5)-------------NOT USED FOR LOOPING
+		
+		for(int i=1;i<=5;i++) {
+			
+			if(i%2==0) {
+				
+				
+				System.out.println("Even: "+i);
+			
+				
+			}
+			else if(i%2==1) {
+				
+				System.out.println("Odd :"+i);
+			}
+		}
+
+	}
+
+}
